8ac5617589820d90434ff279ddb181e17093e766
denisamihut <denisa.m_19@yahoo.com>
2023-04-21 10:21:24 +0300

    created components

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+  @Input() text!: string;
+  @Input() color!: string;
+
+  onClick() {
+    console.log('Add');
+  }
+}
